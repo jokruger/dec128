f7c91874d800c65e769d985851cfce8634edc754
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-01-06 21:32:23 +0200

    round htz

--- rounding.go
+++ rounding.go
@@ -4,6 +4,42 @@ import (
 	"github.com/jokruger/dec128/errors"
 )
 
+// RoundHalfTowardZero rounds the decimal to the specified prec using Half Toward Zero method (https://en.wikipedia.org/wiki/Rounding#Rounding_half_toward_zero).
+//
+// Examples:
+//
+//	Round(1.12345, 4) = 1.1234
+//	Round(1.12335, 4) = 1.1233
+//	Round(1.5, 0) = 1
+//	Round(-1.5, 0) = -1
+func (self Dec128) RoundHalfTowardZero(prec uint8) Dec128 {
+	if self.err != errors.None {
+		return self
+	}
+
+	if prec >= self.exp {
+		return self
+	}
+
+	factor := pow10[self.exp-prec]
+	half := factor / 2
+
+	q, r, err := self.coef.QuoRem64(factor)
+
+	if err != errors.None {
+		return NaN(err)
+	}
+
+	if half < r {
+		q, err = q.Add64(1)
+		if err != errors.None {
+			return NaN(err)
+		}
+	}
+
+	return Dec128{coef: q, exp: prec, neg: self.neg}
+}
+
 // Ceil returns the smallest integer value greater than or equal to 'self'.
 func (self Dec128) Ceil() Dec128 {
 	if self.err != errors.None {
