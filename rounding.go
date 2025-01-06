78f5b2a5a4860dc3092c2a3a9ada5670e6b409ce
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-01-06 19:25:51 +0200

    ceil

--- rounding.go
+++ rounding.go
@@ -4,6 +4,31 @@ import (
 	"github.com/jokruger/dec128/errors"
 )
 
+// Ceil returns the smallest integer value greater than or equal to 'self'.
+func (self Dec128) Ceil() Dec128 {
+	if self.err != errors.None {
+		return self
+	}
+
+	if self.exp == 0 {
+		return self
+	}
+
+	q, r, err := self.coef.QuoRem64(pow10[self.exp])
+	if err != errors.None {
+		return NaN(err)
+	}
+
+	if !self.neg && r != 0 {
+		q, err = q.Add64(1)
+		if err != errors.None {
+			return NaN(err)
+		}
+	}
+
+	return Dec128{coef: q, exp: 0, neg: self.neg}
+}
+
 // Trunc returns self after truncating the decimal to the specified precision.
 //
 // Examples:
