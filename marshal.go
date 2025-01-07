d995c45a585b25fa6d68b52cfc3e0d387a6b4e42
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-01-07 13:06:09 +0200

    scan/value

--- marshal.go
+++ marshal.go
@@ -1,6 +1,10 @@
 package dec128
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql/driver"
+	"fmt"
+)
 
 func (self Dec128) MarshalText() ([]byte, error) {
 	return []byte(self.String()), nil
@@ -44,4 +48,33 @@ func (self *Dec128) UnmarshalJSON(data []byte) error {
 	*self = t
 
 	return nil
+}
+
+func (self *Dec128) Scan(src any) error {
+	var err error
+	switch v := src.(type) {
+	case string:
+		*self = FromString(v)
+		if self.IsNaN() {
+			err = self.ErrorDetails()
+		}
+	//case int64:
+	//	*d, err = NewFromInt64(v, 0)
+	//case int:
+	//	*d, err = NewFromInt64(int64(v), 0)
+	//case int32:
+	//	*d, err = NewFromInt64(int64(v), 0)
+	//case float64:
+	//	*d, err = NewFromFloat64(v)
+	case nil:
+		*self = Zero
+	default:
+		err = fmt.Errorf("can't scan %T to Dec128: %T is not supported", src, src)
+	}
+
+	return err
+}
+
+func (self Dec128) Value() (driver.Value, error) {
+	return self.String(), nil
 }
