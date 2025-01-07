864549c9e283026fbc2aa7a56f5b476431605f2a
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-01-07 12:59:07 +0200

    json marshal/unmarshal

--- marshal.go
+++ marshal.go
@@ -1,1 +1,47 @@
 package dec128
+
+import "bytes"
+
+func (self Dec128) MarshalText() ([]byte, error) {
+	return []byte(self.String()), nil
+}
+
+func (self *Dec128) UnmarshalText(data []byte) error {
+	if len(data) == 0 {
+		*self = Zero
+		return nil
+	}
+
+	t := FromString(string(data[:]))
+	if t.IsNaN() {
+		return t.ErrorDetails()
+	}
+	*self = t
+
+	return nil
+}
+
+func (self Dec128) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + self.String() + `"`), nil
+}
+
+var nullValue = []byte("null")
+
+func (self *Dec128) UnmarshalJSON(data []byte) error {
+	if len(data) >= 2 && data[0] == '"' && data[len(data)-1] == '"' {
+		data = data[1 : len(data)-1]
+	}
+
+	if len(data) == 0 || bytes.Equal(data, nullValue) {
+		*self = Zero
+		return nil
+	}
+
+	t := FromString(string(data[:]))
+	if t.IsNaN() {
+		return t.ErrorDetails()
+	}
+	*self = t
+
+	return nil
+}
