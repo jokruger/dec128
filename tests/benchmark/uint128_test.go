ac41d4eb3ef0309e74fbc1b326656e8d43f36bda
Yurii Biurher <yurii.biurher@zorallabs.com>
2025-04-09 09:52:32 +0300

    nc

--- tests/benchmark/uint128_test.go
+++ tests/benchmark/uint128_test.go
@@ -16,3 +16,43 @@ func BenchmarkUint128FromString(b *testing.B) {
 		_, _ = uint128.FromString(s3)
 	}
 }
+
+func BenchmarkUint128StringConv(b *testing.B) {
+	strs := []string{
+		"1234567890",
+		"12345678901234567890",
+		"1234567890123456789012345678901234567890",
+	}
+	vals := make([]uint128.Uint128, len(strs))
+	for i, s := range strs {
+		vals[i], _ = uint128.FromString(s)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, v := range vals {
+			s := v.String()
+			_, _ = uint128.FromString(s)
+		}
+	}
+}
+
+func BenchmarkUint128BigIntConv(b *testing.B) {
+	strs := []string{
+		"1234567890",
+		"12345678901234567890",
+		"1234567890123456789012345678901234567890",
+	}
+	vals := make([]uint128.Uint128, len(strs))
+	for i, s := range strs {
+		vals[i], _ = uint128.FromString(s)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, v := range vals {
+			b := v.BigInt()
+			_, _ = uint128.FromBigInt(b)
+		}
+	}
+}
